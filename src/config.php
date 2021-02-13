6b6e2cb3413a9fb40b131abb8649b20c6ff5325e
Duduf <git@duduf.com>
2021-02-13 12:08:25 +0100

    initial commit

---
+++ src/config.php
@@ -0,0 +1,26 @@
+<?php
+	/*
+		RAMSES
+		Rainbox Asset Manager
+		Configuration file
+	*/
+
+
+	//SQL
+	//Fill in your SQL Server configuration
+	//host
+	$sqlHost = "localhost";
+	//database name
+	$sqlDBName = "ramtest";
+	//user
+	$sqlUser = "root";
+	//password
+	$sqlpassword = "";
+	//table prefix
+	$tablePrefix = "";
+
+	//SESSION
+	//timeout (seconds)
+	$sessionTimeout = 1200;
+
+?>
