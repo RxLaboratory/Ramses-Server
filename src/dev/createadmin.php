59c16f2652ec2b88a44fa23c42194f2b30514fb8
Duduf <git@duduf.com>
2021-09-15 16:28:55 +0200

    Update createadmin.php

--- src/dev/createadmin.php
+++ src/dev/createadmin.php
@@ -10,6 +10,7 @@
     echo ( "Connecting to the database...<br />" );
 
     include('../config.php');
+    include('../config_security.php');
     include('../functions.php');
     include('../db.php');
 
@@ -22,7 +23,8 @@
     //Setup admin user
     $uuid = uuid();
     $shortName = "Admin";
-    $name = "Administrator";
+    $name = encrypt("Administrator");
+    $email = encrypt("");
     $pswd = hashPassword("0b17bfa7938d75031d1754ab56c27062d967e92ca04f2ba5b4ebf920528936b95f9a9fc96a2ef8fb921463cd97aa94026079891f6f4c6e273ce5956c9da72c92", $uuid);   
     $comment = "The default Administrator user. Don't forget to rename it and change its password!";
     $role = hashRole( 'admin' );
@@ -34,14 +36,16 @@
             `uuid`,
             `password`,
             `role`,
-            `comment`)
+            `comment`,
+            `email` )
         VALUES (
             :name ,
             :shortName ,
             :uuid,
             :password,
             :role,
-            :comment );
+            :comment,
+            :email );
         COMMIT;";
 
     $rep = $db->prepare($qString);
@@ -51,6 +55,7 @@
     $rep->bindValue(':password', $pswd, PDO::PARAM_STR);
     $rep->bindValue(':comment', $comment, PDO::PARAM_STR);
     $rep->bindValue(':role', $role, PDO::PARAM_STR);
+    $rep->bindValue(':email', $email, PDO::PARAM_STR);
 
     $ok = $rep->execute();
     $rep->closeCursor();
