dd5c48b7105b00ede894c5961ffe06be774a0850
Duduf <git@duduf.com>
2022-11-09 12:55:18 +0100

    implement single-object pull

--- src/pull.php
+++ src/pull.php
@@ -24,8 +24,80 @@
         If not, see http://www.gnu.org/licenses/.
     */
 
-    if ( acceptReply( "push" ) )
+    if ( acceptReply( "pull" ) )
     {
+        // There may be a uuid and a table for a single-object pull
+        $table = getArg("table");
 
+        if ($table == "")
+        {
+            $reply["success"] = false;
+            $reply["message"] = "Malformed request, sorry. The table name is missing.";
+            printAndDie();
+        }
+
+        $uuid = getArg("uuid");
+
+        // Single object pull
+        if ($uuid != "")
+        {
+            // Create the table if it doesn't exists
+            createTable( $table );
+
+            $qStr = "SELECT `uuid`, `data`, `modified`, `removed`, `project` ";
+            if ($table == "RamUser") $qStr = $qStr . ", `userName` ";
+            $qStr = $qStr . " FROM `{$tablePrefix}{$table}` WHERE `uuid` = :uuid ;";
+
+            $q = new DBQuery();
+            $q->prepare($qStr);
+            $q->bindStr("uuid", $uuid);
+            $q->execute();
+
+            $r = $q->fetch();
+            if ($r)
+            {
+                $reply["content"]["uuid"] = $r["uuid"];
+                $reply["content"]["modified"] = $r["modified"];
+                $reply["content"]["removed"] = (int)$r["removed"];
+                $reply["content"]["project"] = $r["project"];
+
+                if ($table == "RamUser")
+                {
+                    $reply["content"]["userName"] = $r["userName"];
+                    // We need to decrypt the user data
+                    $data = $r["data"];
+                    $reply["content"]["data"] = decrypt( $data );
+                }
+                else
+                {
+                    $reply["content"]["data"] = $r["data"];
+                }
+
+                $reply["success"] = true;
+                $reply["message"] = "Pulled data: OK!";
+            }
+            else
+            {
+                $reply["content"]["uuid"] = "";
+                $reply["content"]["modified"] = "";
+                $reply["content"]["removed"] = 0;
+                $reply["content"]["project"] = "";
+                $reply["content"]["data"] = "";
+
+                if ($table == "RamUser") $reply["content"]["userName"] = "";
+
+                $reply["success"] = true;
+                $reply["message"] = "Pulled data: this object doesn't exist.";
+            }
+
+            $q->close();
+
+            printAndDie();
+        }
+
+        // The page to send
+        $page = getArg("page", 1);
+
+        // Pull the 
     }
 ?>
