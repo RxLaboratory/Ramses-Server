9268c3abc7212611d9d4f36627da72ba199dbe73
Duduf <git@duduf.com>
2022-12-08 18:01:02 +0100

    v0.8.1

--- src/global.php
+++ src/global.php
@@ -24,7 +24,7 @@
     // GLOBAL VARIABLES AND SETTINGS //
 
     // The version
-    $ramsesVersion = "0.8.0-Beta";
+    $ramsesVersion = "0.8.1-Beta";
 
     // Settings
     // Number of pages returned by fetch and pull
